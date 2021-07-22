33eec2c6c3f66c6f1e79151d98725f3608d141af
inch <sxin2949@naver.com>
2021-07-22 16:54:08 +0900

    #2.0 Movies Controller

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
      새로운 컨트롤러를 만들 거면 npm g co 라고 친다음에 컨트롤러 이름을 만들면 됨. 
      이렇게 하면 자동으로 app.modules 파일에 import 시켜줌. 
      spec 파일은 테스트 파일이라 지워도 됨. 
+
+#2.2 만약 create를 해서 결과값을 받고 싶다면 Decorator를 활용하면 된다. 
+     만약 get으로 인자를 받을 거면 get 중에 맨 아래로 써줘야 한다. 
+     아니면 그 인자값이 다들어간다고 생각하고 의도치 않게 인자를 요구하게 된다. 
+     @Get("/search")에서 @Query는 ~/movies/search/year=3000 같은 거고
+     @Get("/:id")는 ~/movies/search/1 같은 거다. 
--- hinext/src/movies/movies.controller.ts
+++ hinext/src/movies/movies.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Param, Post, Patch } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Patch, Body, Query } from '@nestjs/common';
 
 @Controller('movies')  // 이 부분 때문에 localhost:3000/movies 파일이 됨. router 같은 거임. 
 export class MoviesController {
@@ -7,6 +7,11 @@ export class MoviesController {
     getAll(){
         return "This will return all movies";
     }
+    
+    @Get("/search")
+    search(@Query('year') searchYear: string){
+        return `We are searching for a movie with a title: ${searchYear}`;
+    }
 
     @Get("/:id") 
     getOne(@Param("id") movieId: string){  //@parm("id")는 위에 아이디와 같아야 하나 뒤에 오는 것은 달라도 됨. 
@@ -14,8 +19,9 @@ export class MoviesController {
     }
 
     @Post() 
-    create() {
-        return "This will create a movie";
+    create(@Body() movieData) {
+        console.log(movieData);
+        return movieData;
     }
 
     @Delete("/:id")
@@ -24,7 +30,10 @@ export class MoviesController {
     } 
 
     @Patch("/:id") // patch는 리소스의 일부분만 업데이트 해준다. 
-    patch(@Param('id') movieId: string) {
-        return `This will patch a movie with the id: ${movieId}`;
+    patch(@Param('id') movieId: string, @Body() updateData) {
+        return {
+            updataedMovie: movieId, 
+            ...updateData,
+        }
     }
 }
