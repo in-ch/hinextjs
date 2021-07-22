664ed751ad45609c4caba433d85b971f23cf8756
inch <sxin2949@naver.com>
2021-07-22 15:50:54 +0900

    #1.1 Controllers

--- hinext/src/main.ts
+++ hinext/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap() { // 함수 이름이니깐 바꿀 수 있음.
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
