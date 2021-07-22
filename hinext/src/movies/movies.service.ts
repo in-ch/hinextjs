964cbb8e4eb6d7e9dd5ec6e869859c96a7daa2e4
inch <sxin2949@naver.com>
2021-07-22 19:01:00 +0900

    #2.7 Express on NestJS

--- hinext/src/movies/movies.service.ts
+++ hinext/src/movies/movies.service.ts
@@ -3,7 +3,7 @@ import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movies.entity';
 
-@Injectable()
+@Injectable()  // decorator
 export class MoviesService {
     private movies: Movie[] = [];
 
