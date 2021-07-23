89aed77ba08433e32279034236c7ab7edc346498
inch <sxin2949@naver.com>
2021-07-23 11:26:49 +0900

    #3.2 Testing getAll and getOne

--- hinext/src/movies/movies.service.spec.ts
+++ hinext/src/movies/movies.service.spec.ts
@@ -15,4 +15,8 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it("should be 4", () => {    // it이라고 한 다음에 이름 적어주고 
+    expect(2+2).toEqual(4);    // expect는 검사할 모듈이나 함수 등을 적는 곳 
+  })
 });
